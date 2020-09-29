6f48b38edfdd6c26cd5c47176c4adeddcd1f3024
TejashDatta <tejashdatta@gmail.com>
2020-09-29 12:43:03 +0530

    cdと一時変数を排除

--- shell/checkpoint_indexed_files.sh
+++ shell/checkpoint_indexed_files.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-NUMBER=$1
-
 if [[ $1 == 0 || ! $1 =~ ^[0-9]+$ ]]; then
   echo "0以上の整数だけを入力してください。"
   exit 1
 fi
 
 mkdir out
-cd out
-for i in $(seq "$NUMBER"); do
-  NEW_DIRECTORY=dir-$i
-  mkdir "$NEW_DIRECTORY"
-  cd "$NEW_DIRECTORY"
+for i in $(seq $1); do
+  mkdir "out/dir-$i"
   for j in $(seq $i); do
-    touch "file-$j"
+    touch "out/dir-$i/file-$j"
   done
-  cd ..
 done
