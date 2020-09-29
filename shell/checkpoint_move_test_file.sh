cb600301dcc328309ab1372ad30780374078d524
TejashDatta <tejashdatta@gmail.com>
2020-09-29 12:23:39 +0530

    ガード筋に変更

--- shell/checkpoint_move_test_file.sh
+++ shell/checkpoint_move_test_file.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [[ -d child-directory-a && -d child-directory-b ]]; then
-  touch child-directory-a/test.txt
-  cd child-directory-a
-  mv test.txt ../child-directory-b
-  cd ../child-directory-b
-  mv test.txt ../
-else
+if [[ ! ( -d child-directory-a && -d child-directory-b ) ]]; then
   echo "エラー：child-directory-aとchild-directory-bのディレクトリはなければなりません。"
+  exit 1
 fi
+
+touch child-directory-a/test.txt
+cd child-directory-a
+mv test.txt ../child-directory-b
+cd ../child-directory-b
+mv test.txt ../
