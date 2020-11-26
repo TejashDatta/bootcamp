99371e98f894e726fcf88864d19b7e051430c581
Tejash Datta <tejashdatta@gmail.com>
2020-11-27 00:11:16 +0530

    rgb exercise completed

---
+++ ruby-book/exercises/rgb/tests/rgb_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require_relative '../lib/rgb'
+
+class RGBTest < Minitest::Test
+    def test_to_hex
+        assert_equal '#000000', to_hex(0, 0, 0)
+        assert_equal '#FFFFFF', to_hex(255, 255, 255)
+    end
+
+    def test_to_ints
+        assert_equal [0, 0, 0], to_ints('#000000')
+        assert_equal [255, 255, 255], to_ints('#FFFFFF')
+    end
+end
