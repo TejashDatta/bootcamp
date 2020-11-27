35c03c0bca1e57aa2e22910c02b38667c9d754ca
Tejash Datta <tejashdatta@gmail.com>
2020-11-27 10:10:55 +0530

    format fix

--- ruby-book/sample_test.rb
+++ ruby-book/sample_test.rb
@@ -1,16 +1,16 @@
 require 'minitest/autorun'
 
 class SampleTest < Minitest::Test
-    def test_upcase
-        assert_equal 'TEST', 'test'.upcase
-        assert_equal 'HELLO', 'hello'.upcase
-        assert 'HELLO' == 'hello'.upcase
-        refute 'HELLO' == 'hello'.downcase
-    end
+  def test_upcase
+    assert_equal 'TEST', 'test'.upcase
+    assert_equal 'HELLO', 'hello'.upcase
+    assert 'HELLO' == 'hello'.upcase
+    refute 'HELLO' == 'hello'.downcase
+  end
 
-    def test_downcase
-        assert_equal 'test', 'Tesesst'.wrongcase
-        assert_equal 'test', 'Tesesst'.downcase
-        assert_equal 'test', 'Tese'.downcase
-    end
+  def test_downcase
+    assert_equal 'test', 'Tesesst'.wrongcase
+    assert_equal 'test', 'Tesesst'.downcase
+    assert_equal 'test', 'Tese'.downcase
+  end
 end
