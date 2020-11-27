9e7cf96d2d08ebca58fef9e441495f3904780482
Tejash Datta <tejashdatta@gmail.com>
2020-11-27 10:15:52 +0530

    indent fix

--- ruby-book/exercises/rgb/lib/rgb.rb
+++ ruby-book/exercises/rgb/lib/rgb.rb
@@ -1,10 +1,10 @@
 def to_hex(red, green, blue)
-    [red, green, blue].reduce('#') do |hexadecimal_color_code, color_value|
-        hexadecimal_color_code += color_value.to_s(16).rjust(2, '0').upcase
-    end
+  [red, green, blue].reduce('#') do |hexadecimal_color_code, color_value|
+    hexadecimal_color_code += color_value.to_s(16).rjust(2, '0').upcase
+  end
 end
 
 def to_ints(hexadecimal_color_code)
-    *color_values = hexadecimal_color_code[1, 2], hexadecimal_color_code[3, 2], hexadecimal_color_code[5, 2]
-    color_values.map(&:hex)
+  *color_values = hexadecimal_color_code[1, 2], hexadecimal_color_code[3, 2], hexadecimal_color_code[5, 2]
+  color_values.map(&:hex)
 end
