626c2be46f21a7e756a35c54be8af64b98dba63a
Tejash Datta <tejashdatta@gmail.com>
2020-12-22 11:05:18 +0530

    space added

--- javascript/tutorial/array-sort.js
+++ javascript/tutorial/array-sort.js
@@ -6,9 +6,9 @@ var points = [40, 100, 1, 5, 25, 10];
 points.sort(function(a, b){return a - b});
 
 var cars = [
-  {type:"Volvo", year:2016},
-  {type:"Saab", year:2001},
-  {type:"BMW", year:2010}
+  {type: "Volvo", year: 2016},
+  {type: "Saab", year: 2001},
+  {type: "BMW", year: 2010}
 ];
 
 cars.sort(function(a, b){return a.year - b.year});
