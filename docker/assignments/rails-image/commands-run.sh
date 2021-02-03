a6d7914f3c2f43ccc2b97a140f3ae0c6633c5a4a
Tejash Datta <tejashdatta@gmail.com>
2021-02-04 02:08:59 +0530

    changed to add postgres db server

--- docker/assignments/rails-image/commands-run.sh
+++ docker/assignments/rails-image/commands-run.sh
@@ -1,1 +1,14 @@
-rails new example-app
+docker-compose run --no-deps web rails new . --force --database=postgresql
+
+sudo chown -R $USER:$USER .
+
+docker-compose build
+
+# in config/database.yml add:
+# host: db
+#   username: postgres
+#   password: password
+
+docker-compose up
+
+docker-compose run web rake db:create
