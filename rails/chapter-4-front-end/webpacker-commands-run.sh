f63d0a3e86e7d782536dd48af2ad89438cfa2424
Tejash Datta <tejashdatt@gmail.com>
2021-02-28 03:53:59 +0530

    js library loader plugin

---
+++ rails/chapter-4-front-end/webpacker-commands-run.sh
@@ -0,0 +1,17 @@
+rails new webpacker_sample
+rails g scaffold user name
+rails db:create
+rails db:migrate
+
+rails webpacker:compile
+
+yarn add jquery
+
+bin/webpack-dev-server
+
+# 出力割愛
+rails new react_example --webpack=react
+
+rails webpacker:install:react
+
+rails webpacker:install:erb
