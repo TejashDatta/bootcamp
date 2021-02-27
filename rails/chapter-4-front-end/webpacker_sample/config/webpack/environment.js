c4ccbafa189c15a467a55ff29fd18e05d5e7f487
Tejash Datta <tejashdatt@gmail.com>
2021-02-28 04:06:16 +0530

    undo

--- rails/chapter-4-front-end/webpacker_sample/config/webpack/environment.js
+++ rails/chapter-4-front-end/webpacker_sample/config/webpack/environment.js
@@ -1,7 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const erb = require('./loaders/erb')
-const provide = require('./plugins/provide')
 
-environment.loaders.prepend('erb', erb)
-environment.loaders.prepend('provide', provide)
 module.exports = environment
