53c113896e42994ca28366dbd13b29c2ad0ed44b
Tejash Datta <tejashdatt@gmail.com>
2021-03-07 16:39:13 +0530

    sendgrid and test

--- rails/chapter-5-rails-standard-functionality/5-3-action-mailer/action_mailer_app/config/environments/development.rb
+++ rails/chapter-5-rails-standard-functionality/5-3-action-mailer/action_mailer_app/config/environments/development.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :file
+  # config.action_mailer.delivery_method = :file
   # config.action_mailer.file_settings = { location: Rails.root.join('log/mails') }
-  
+  config.action_mailer.delivery_method = :smtp
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- rails/chapter-5-rails-standard-functionality/5-3-action-mailer/action_mailer_app/test/mailers/user_mailer_test.rb
+++ rails/chapter-5-rails-standard-functionality/5-3-action-mailer/action_mailer_app/test/mailers/user_mailer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'welcome' do
+    email = UserMailer.with(to: 'tejash@eg.com', name: 'tejash').welcome
+    assert_emails(1) { email.deliver_now }
+    assert_equal ['perfect_rails@example.com'], email.from
+    assert_equal ['tejash@eg.com'], email.to
+    assert_equal 'Registration complete', email.subject
+    assert_includes email.text_part.decoded, 'tejash'
+    assert_includes email.html_part.decoded, 'tejash'
+  end
 end
