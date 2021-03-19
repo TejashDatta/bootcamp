264301411b4987cf09cba4c9be0e20ead3b6a25e
Tejash Datta <tejashdatt@gmail.com>
2021-03-20 02:24:27 +0530

    functional and model testing

--- rails/chapter-6-awesome-events-and-chapter-7-application-testing/awesome_events/test/controllers/events_controller_test.rb
+++ rails/chapter-6-awesome-events-and-chapter-7-application-testing/awesome_events/test/controllers/events_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'delete own event' do
+    event_owner = FactoryBot.create(:user)
+    event = FactoryBot.create(:event, owner: event_owner)
+    sign_in_as event_owner
+    assert_difference('Event.count', -1) do
+      delete event_url(event)
+    end
+  end
+
+  test "cannot delete other's event" do
+    event = FactoryBot.create(:event, owner: FactoryBot.create(:user))
+    sign_in_as FactoryBot.create(:user)
+    assert_difference('Event.count', 0) do
+      assert_raises(ActiveRecord::RecordNotFound) do
+        delete event_url(event)
+      end
+    end
+  end
 end
--- rails/chapter-6-awesome-events-and-chapter-7-application-testing/awesome_events/test/models/event_test.rb
+++ rails/chapter-6-awesome-events-and-chapter-7-application-testing/awesome_events/test/models/event_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#created_by? when owner_id same as #id parameter stub' do
+    event = FactoryBot.create(:event)
+    user = User.new
+    user.stub(:id, event.owner_id) do
+      assert_equal(true, event.created_by?(user))
+    end
+  end
+
+  test '#created_by? when owner_id same as #id parameter mock' do
+    event = FactoryBot.create(:event)
+    user = MiniTest::Mock.new.expect(:id, event.owner_id)
+    assert_equal(true, event.created_by?(user))
+    user.verify
+  end
+
+  test '#created_by? when owner_id different from #id parameter' do
+    assert_equal(false, FactoryBot.create(:event).created_by?(FactoryBot.create(:user)))
+  end
+
+  test '#created_by? when parameter is nil' do
+    assert_equal(false, FactoryBot.create(:event).created_by?(nil))
+  end
+
+  test 'start_at_should_be_before_end_at validation OK' do
+    start_at = rand(1..30).days.from_now
+    event = FactoryBot.build(:event,
+                             start_at: start_at,
+                             end_at: start_at + rand(1..30).hours)
+    event.valid?
+    assert_empty(event.errors[:start_at])
+  end
+
+  test 'start_at_should_be_before_end_at validation error' do
+    start_at = rand(1..30).days.from_now
+    event = FactoryBot.build(:event,
+                             start_at: start_at,
+                             end_at: start_at - rand(1..30).hours)
+    event.valid?
+    assert_not_empty(event.errors[:start_at])
+  end
 end
