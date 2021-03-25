1aa9bba02607a3107e08e365da5d6b9930362dfe
Tejash Datta <tejashdatt@gmail.com>
2021-03-25 12:51:31 +0530

    rubocop and brakeman

---
+++ rails/chapter-9-code-quality-improvement/9-3-rubocop/commands.sh
@@ -0,0 +1,7 @@
+gem install rubocop
+
+rubocop example.rb
+# correct all
+rubocop -a example.rb
+# analyse and correct lint cops
+rubocop -l example.rb
---
+++ rails/chapter-9-code-quality-improvement/9-3-rubocop/example.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+puts({ name: 'Hello', description: 'I say hello' })
