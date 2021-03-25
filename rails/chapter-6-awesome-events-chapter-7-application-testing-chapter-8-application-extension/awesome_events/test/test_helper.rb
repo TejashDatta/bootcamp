eb7ff15d1c7941b80c946514cbfbbbd6513cbc50
Tejash Datta <tejashdatt@gmail.com>
2021-03-25 13:32:35 +0530

    install simplecov

--- rails/chapter-6-awesome-events-chapter-7-application-testing-chapter-8-application-extension/awesome_events/test/test_helper.rb
+++ rails/chapter-6-awesome-events-chapter-7-application-testing-chapter-8-application-extension/awesome_events/test/test_helper.rb
@@ -1,4 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'minitest/mock'
@@ -6,7 +10,7 @@ require_relative 'sign_in_helper'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
 end
