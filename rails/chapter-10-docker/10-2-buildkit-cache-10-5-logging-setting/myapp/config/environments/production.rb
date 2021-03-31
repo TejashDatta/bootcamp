c9d4be4ea6e0241e127a33fe7af3511e52c92f19
Tejash Datta <tejashdatt@gmail.com>
2021-03-31 12:29:44 +0530

    remove logging

--- rails/chapter-10-docker/10-2-buildkit-cache-10-5-logging-setting/myapp/config/environments/production.rb
+++ rails/chapter-10-docker/10-2-buildkit-cache-10-5-logging-setting/myapp/config/environments/production.rb
@@ -80,9 +80,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.logger = ActiveSupport::Logger.new($stdout)
-  $stdout.sync = true 
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
