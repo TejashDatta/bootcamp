b62960c4f9b7d7b0b6eef4eaac781a0751bc97ad
Tejash Datta <tejashdatt@gmail.com>
2021-04-16 18:50:17 +0530

    with callback object without concern

---
+++ rails/expert-rails/13-2-callback-object/encryption_callback_sample/app/callbacks/attribute_encryption_callbacks.rb
@@ -0,0 +1,20 @@
+class AttributeEncryptionCallbacks
+  def initialize(attribute)
+    @attribute = attribute
+  end
+
+  def self.encryptor
+    @attribute_encryptor ||= Rails.application.key_generator
+                                  .generate_key('attribute encryptor', ActiveSupport::MessageEncryptor.key_len)
+                                  .then { |key| ActiveSupport::MessageEncryptor.new(key, serializer: JSON) }
+  end
+
+  def after_save(record)
+    record[@attribute] = self.class.encryptor.decrypt_and_verify(record[@attribute])
+  end
+  alias after_find after_save
+
+  def before_save(record)
+    record[@attribute] = self.class.encryptor.encrypt_and_sign(record[@attribute])
+  end
+end
